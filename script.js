ee1485c703a094404ac29904c06b8ba2420279ce
HILLARY  NTHOME <58300609+hillary97@users.noreply.github.com>
2025-03-07 17:11:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function graphics(){
+			
+			var conte=document.getElementById('areas-of-expertise').innerHTML="<p>Figma, Canva, Adobe Photoshop, Illustrator, CoreDraw</p>";
+		}
+function languages(){
+			
+			var conte=document.getElementById('areas-of-expertise').innerHTML="<p>Python, Html, CSS, Bootstrap, PHP, Laravel, C++, C, React, Javascript, Kotlin, Django, C#, SQL, MySQL,Asp.net, Java, GIT, Github</p>";
+		}
+	
+function education(){
+	var colorr=document.getElementById("educ").style.color="blue";
+	var other=document.getElementById("workc").style.color="black";
+	var education=document.getElementById("eduwork").innerHTML="<img class='img-fluid mx-auto d-block' src='images/education.png'>";
+}
+function work(){
+	var colorr=document.getElementById("workc").style.color="blue";
+	var othe=document.getElementById("educ").style.color="black";
+	var education=document.getElementById("eduwork").innerHTML="<img class='img-fluid mx-auto d-block' src='images/work.png'>";
+}
+
